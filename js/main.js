041ce926e7b2f0770a8927d60cfb30be931a2c1f
Dylan Dao <dylanpdao@gmail.com>
2023-04-11 20:33:39 +0000

    fixed issues: created li inside renderEntry, moved data-view setters, and changed toggleNoEntries to hide instead of create/insert

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const $dvEntries = document.querySelector('[data-view="entries"]');
 const $dvEntryForm = document.querySelector('[data-view="entry-form"]');
 const $goEntries = document.querySelector('.go-entries');
 const $newBtn = document.querySelector('.new-btn');
+const $noEntry = document.querySelector('.no-entries');
 
 // updates picture when url link is entered
 $urlBox.addEventListener('input', function (e) {
@@ -26,72 +27,59 @@ $form.addEventListener('submit', function (e) {
   data.entries.unshift(journalEntry);
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
-  const $li = document.createElement('li');
-  $li.appendChild(renderEntry(data.entries[0]));
-  $ul.prepend($li);
-  toggleNoEntries();
+  $ul.prepend(renderEntry(journalEntry));
 });
 
 // render entries
 function renderEntry(entry) {
   // made first row
-  const row1 = document.createElement('div');
-  row1.className = 'row';
+  const $row1 = document.createElement('div');
+  $row1.className = 'row';
 
   // div to house image
-  const pictureDiv = document.createElement('div');
-  pictureDiv.className = 'column-half';
-  const imgDiv = document.createElement('img');
-  imgDiv.className = 'img-up';
+  const $pictureDiv = document.createElement('div');
+  $pictureDiv.className = 'column-half';
+  const $imgDiv = document.createElement('img');
+  $imgDiv.className = 'img-up';
 
   // div to house title, and notes
-  const infoDiv = document.createElement('div');
-  infoDiv.className = 'column-half';
-  const titleDiv = document.createElement('div');
-  titleDiv.className = 'label-head li-head';
-  const notesDiv = document.createElement('div');
-  notesDiv.className = 'notesText notes-text';
+  const $infoDiv = document.createElement('div');
+  $infoDiv.className = 'column-half';
+  const $titleDiv = document.createElement('div');
+  $titleDiv.className = 'label-head li-head';
+  const $notesDiv = document.createElement('div');
+  $notesDiv.className = 'notesText notes-text';
 
   // give value to each element
-  imgDiv.setAttribute('src', entry.imgUrl);
-  titleDiv.textContent = entry.title;
-  notesDiv.textContent = entry.notes;
+  $imgDiv.setAttribute('src', entry.imgUrl);
+  $titleDiv.textContent = entry.title;
+  $notesDiv.textContent = entry.notes;
 
-  row1.appendChild(pictureDiv);
-  pictureDiv.appendChild(imgDiv);
-  row1.appendChild(infoDiv);
-  infoDiv.appendChild(titleDiv);
-  infoDiv.appendChild(notesDiv);
+  $row1.appendChild($pictureDiv);
+  $pictureDiv.appendChild($imgDiv);
+  $row1.appendChild($infoDiv);
+  $infoDiv.appendChild($titleDiv);
+  $infoDiv.appendChild($notesDiv);
 
-  return row1;
+  const $li = document.createElement('li');
+  $li.appendChild($row1);
+  return $li;
 }
 
 // append all old entries in
 document.addEventListener('DOMContentLoaded', function (e) {
   for (let i = 0; i < data.entries.length; i++) {
-    const $li = document.createElement('li');
-    $li.appendChild(renderEntry(data.entries[i]));
-    $ul.appendChild($li);
+    $ul.appendChild(renderEntry(data.entries[i]));
   }
   if (data.view === 'entries') {
     viewSwap('entries');
-    toggleNoEntries();
   }
 });
 
 // add text if no entries are present
 function toggleNoEntries() {
-  if (data.entries.length === 0) {
-    const noEntries = document.createElement('div');
-    noEntries.textContent = 'No entries have been recorded';
-    noEntries.className = 'no-entries';
-    const $li = document.createElement('li');
-    $li.appendChild(noEntries);
-    $ul.appendChild($li);
-  }
-  if (data.entries.length !== 0) {
-    const noEntries = document.querySelector('.no-entries');
-    noEntries.remove();
+  if (data.entries.length > 0) {
+    $noEntry.className = 'no-entries hidden';
   }
 }
 
@@ -100,19 +88,20 @@ function viewSwap(view) {
   if (view === $dvEntryForm.dataset.view) {
     $dvEntryForm.className = 'not-hidden';
     $dvEntries.className = 'hidden';
+    data.view = 'entry-form';
   }
   if (view === $dvEntries.dataset.view) {
     $dvEntries.className = 'not-hidden';
     $dvEntryForm.className = 'hidden';
+    data.view = 'entries';
+    toggleNoEntries();
   }
 }
 
 // press buttons to switch views
 $goEntries.addEventListener('click', function (e) {
   viewSwap('entries');
-  data.view = 'entries';
 });
 $newBtn.addEventListener('click', function (e) {
   viewSwap('entry-form');
-  data.view = 'entry-form';
 });
